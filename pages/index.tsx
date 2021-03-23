633c6495c329934b0b840668f0113d4031f609af
STomlinson1 <53631071+STomlinson1@users.noreply.github.com>
2021-03-23 16:25:56 -0400

    Set up server side rendering for home page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,64 @@
+import { GetServerSideProps } from 'next';
+import { useState } from 'react';
 import Head from 'next/head';
+import axios from 'axios';
 
-export default function Home() {
+interface HomeProps {
+	data: Data;
+}
+interface Data {
+	ip: string;
+	location: {
+		country: string;
+		region: string;
+		city: string;
+		lat: number;
+		lng: number;
+		postalCode: string;
+		timezone: string;
+		geonameId: number;
+	};
+	isp: string;
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+	try {
+		const response = await axios.get('https://geo.ipify.org/api/v1', {
+			params: {
+				apiKey: process.env.API_KEY,
+				ipAddress: ''
+			}
+		});
+		console.log(response.data);
+
+		return {
+			props: {
+				data: response.data
+			}
+		};
+	} catch (error) {
+		return {
+			props: {
+				data: undefined
+			}
+		};
+	}
+};
+
+const Home: React.FC<HomeProps> = ({ data }) => {
+	const [ ipData, setIpData ] = useState(data);
+
+	console.log(ipData);
+
 	return (
 		<div>
 			<Head>
-				<title>Create Next App</title>
+				<title>IP Address Tracker</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<h1>Project Start</h1>
 		</div>
 	);
-}
+};
+
+export default Home;
