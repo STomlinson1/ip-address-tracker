e75df8f77f503edd1e53d6cc9c1f50fe9f4d3939
STomlinson1 <53631071+STomlinson1@users.noreply.github.com>
2021-03-23 23:17:06 -0400

    Hook up leaflet map.

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,19 +1,33 @@
 import s from './Header.module.scss';
 import { useState } from 'react';
+import Image from 'next/image';
 
 interface HeaderProps {
 	title: string;
 	placeholder: string;
 }
 
-const Header: React.FC<HeaderProps> = ({ title, placeholder }) => {
+const Header: React.FC<HeaderProps> = ({ title, placeholder, children }) => {
+	const [ term, setTerm ] = useState('');
+
 	return (
 		<header className={s.header}>
 			<h1 className={s.heading}>{title}</h1>
 			<div className={s.container}>
-				<input className={s.input} type="text" placeholder={placeholder} />
-				<button className={s.button}>s</button>
+				<form>
+					<input
+						className={s.input}
+						type="text"
+						placeholder={placeholder}
+						value={term}
+						onChange={(e) => setTerm(e.target.value)}
+					/>
+					<button type="submit" className={s.button}>
+						<Image src="/images/icon-arrow.svg" height={12} width={10} />
+					</button>
+				</form>
 			</div>
+			{children}
 		</header>
 	);
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import { GetServerSideProps } from 'next';
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
 import Head from 'next/head';
 import axios from 'axios';
 
-import { Header } from '../components';
+import { Header, Map } from '../components';
+
+const DEFAULT_CENTER = [ 38.907132, -77.036546 ];
 
 interface HomeProps {
 	data: Data | undefined;
@@ -52,7 +54,7 @@ const Home: React.FC<HomeProps> = ({ data }) => {
 	console.log(ipData);
 
 	return (
-		<div>
+		<Fragment>
 			<Head>
 				<title>IP Address Tracker</title>
 				<link rel="icon" href="/favicon.ico" />
@@ -61,7 +63,22 @@ const Home: React.FC<HomeProps> = ({ data }) => {
 				title="IP Address Tracker"
 				placeholder="Search for any IP address or domain"
 			/>
-		</div>
+			<Map className center={DEFAULT_CENTER} zoom={16}>
+				{({ TileLayer, Marker, Popup }) => (
+					<Fragment>
+						<TileLayer
+							url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+							attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+						/>
+						<Marker position={DEFAULT_CENTER}>
+							<Popup>
+								A pretty CSS3 popup. <br /> Easily customizable.
+							</Popup>
+						</Marker>
+					</Fragment>
+				)}
+			</Map>
+		</Fragment>
 	);
 };
 
